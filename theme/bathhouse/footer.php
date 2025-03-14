5a0eda1ecbabf5b2cde89e32fd293257260160dc
Игорь Гордеев <igor@MacBook-Pro-Igor.local>
2025-03-14 10:04:01 +0300

    нормально добавлена тема, добавлены некоторые файлы

--- theme/bathhouse/footer.php
+++ theme/bathhouse/footer.php
@@ -11,24 +11,25 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bathhouse' ) ); ?>">
-				<?php
+<footer id="colophon" class="site-footer" style="background:#000000;">
+	<div class="site-info">
+		<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bathhouse' ) ); ?>">
+			<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'bathhouse' ), 'WordPress' );
 				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
+		</a>
+		<span class="sep"> | </span>
+		<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'bathhouse' ), 'bathhouse', '<a href="http://underscores.me/">Underscores.me</a>' );
 				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
